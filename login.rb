c4977b67cc6ac016f51fa56984089fe4591944ea
Petr Archakov <iamazriah@gmail.com>
2011-11-29 13:43:10 +0400

    Simple login/first twit console app

---
+++ login.rb
@@ -0,0 +1,43 @@
+require 'oauth'
+require 'twitter'
+
+ctoken = 'gdewglXHW5SuI1xndfnw'
+csecret = '6xphMK1Bkuvi4ZM0PhDR4217b3rxn7PCGy3RxETIVc'
+url = 'http://api.twitter.com'
+
+p 'Login? yes/no'
+login = gets.chomp!
+
+if login == 'yes'
+	oa = OAuth::Consumer.new(ctoken, csecret, :site => url, :request_endpoint => url, :sign_in => true)
+	rt = oa.get_request_token
+	rsecret = rt.secret
+	rtoken = rt.token
+	
+	auth_url = "http://api.twitter.com/oauth/authorize?oauth_token="+rtoken
+	system("firefox "+auth_url)
+	
+	p 'Put yes when login'
+	login = gets.chomp!
+	if login == 'yes'
+		at = rt.get_access_token
+		asecret = at.secret
+		atoken = at.token
+		
+		Twitter.configure do |config|
+			config.consumer_key = ctoken
+			config.consumer_secret = csecret
+			config.oauth_token = atoken
+			config.oauth_token_secret = asecret
+		end
+		
+		client = Twitter::Client.new
+		
+		p 'Put your twit here:'
+		twit = gets.chomp!
+		
+		client.update(twit);
+	end
+end
+
+p 'Finish!'
